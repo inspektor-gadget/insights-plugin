2f07f569ed8a588c785f4f018a5dcfcae494fc9f
Michael Friese <mfriese@microsoft.com>
2026-05-15 12:08:33 +0200

    enh: slim down gadget view further when in guided instances
    Signed-off-by: Michael Friese <mfriese@microsoft.com>

--- src/components/projects/DnsTab.tsx
+++ src/components/projects/DnsTab.tsx
@@ -1,7 +1,15 @@
+import { ViewConfig } from '@inspektor-gadget/ig-desktop/frontend';
 import React from 'react';
 import ProjectGadgetTab from './ProjectGadgetTab';
 
 const GADGET_IMAGE = 'ghcr.io/inspektor-gadget/gadget/trace_dns:latest';
+
+const EMBEDDED_VIEW_CONFIG: ViewConfig = {
+  inspector: false,
+  logPanel: false,
+  searchBar: true,
+  snapshotTimeline: false,
+};
 
 interface DnsTabProps {
   project: {
@@ -13,6 +21,12 @@ interface DnsTabProps {
 
 export default function DnsTab({ project }: DnsTabProps) {
   return (
-    <ProjectGadgetTab project={project} gadgetImage={GADGET_IMAGE} gadgetLabel="DNS" embedded />
+    <ProjectGadgetTab
+      project={project}
+      gadgetImage={GADGET_IMAGE}
+      gadgetLabel="DNS"
+      viewConfig={EMBEDDED_VIEW_CONFIG}
+      embedded
+    />
   );
 }
--- src/components/projects/NetworkTab.tsx
+++ src/components/projects/NetworkTab.tsx
@@ -1,7 +1,15 @@
+import { ViewConfig } from '@inspektor-gadget/ig-desktop/frontend';
 import React from 'react';
 import ProjectGadgetTab from './ProjectGadgetTab';
 
 const GADGET_IMAGE = 'ghcr.io/inspektor-gadget/gadget/trace_tcp:latest';
+
+const EMBEDDED_VIEW_CONFIG: ViewConfig = {
+  inspector: false,
+  logPanel: false,
+  searchBar: true,
+  snapshotTimeline: false,
+};
 
 interface NetworkTabProps {
   project: {
@@ -13,6 +21,12 @@ interface NetworkTabProps {
 
 export default function NetworkTab({ project }: NetworkTabProps) {
   return (
-    <ProjectGadgetTab project={project} gadgetImage={GADGET_IMAGE} gadgetLabel="Network" embedded />
+    <ProjectGadgetTab
+      project={project}
+      gadgetImage={GADGET_IMAGE}
+      gadgetLabel="Network"
+      viewConfig={EMBEDDED_VIEW_CONFIG}
+      embedded
+    />
   );
 }
